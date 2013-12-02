95a5b2d221fb9d8e6b0e7521924ade0e7682668f
Abby Pan <abbypan@gmail.com>
2013-12-02 15:58:39 +0800

    write_table,  merge

--- lib/SimpleR/Reshape.pm
+++ lib/SimpleR/Reshape.pm
@@ -3,10 +3,11 @@ package SimpleR::Reshape;
 
 require Exporter;
 @ISA    = qw(Exporter);
-@EXPORT = qw( read_table melt cast );
+@EXPORT = qw(read_table write_table melt cast merge);
 
 our $VERSION     = 0.02;
 our $DEFAULT_SEP = ',';
+
 
 sub read_table {
     my ( $txt, %opt ) = @_;
@@ -55,6 +56,7 @@ sub read_table {
     return \@data;
 }
 
+
 sub gen_sub_write_row {
     my ( $txt, %opt ) = @_;
     $opt{sep} ||= $DEFAULT_SEP;
@@ -62,10 +64,26 @@ sub gen_sub_write_row {
     my $write_format = $opt{charset} ? ">:$opt{charset}" : ">";
     open my $fh, $write_format, $txt;
 
+    if($opt{header}){
+        print $fh join($opt{sep}, @{$opt{header}}), "\n";
+    }
+
     my $w_sub = sub {
-        print $fh join( $opt{sep}, @{ $_[0] } ), "\n";
+        my ($r) = @_;
+
+        #支持嵌套一层ARRAY
+        my @data = map {  ref($_) eq 'ARRAY' ? @$_ : $_ } @$r;
+
+        print $fh join( $opt{sep}, @data ), "\n";
     };
     return $w_sub;
+}
+
+sub write_table {
+    my ($data, %opt) = @_;
+    my $w_sub = gen_sub_write_row($opt{file}, %opt);
+    $w_sub->($_) for @$data;
+    return $opt{file};
 }
 
 sub melt {
@@ -107,14 +125,15 @@ sub cast {
             $kv{$k} = \%temp;
         }
 
-        my $v_name = $r->[ $opt{measure} ];
+        my $v_name = $r->[ $opt{measure} ] // 'ALL';
         $measure_name{$v_name} = 1;
-        my $v = $r->[ $opt{value} ];
+
+        my $v =  $r->[ $opt{value} ];
         push @{ $kv{$k}{$v_name} }, $v;
 
         if(exists $opt{reduce_sub}){
             my $tmp = $opt{reduce_sub}->($kv{$k}{$v_name});
-            $kv{$k}{$v_name} = [ $tmp ];
+            $kv{$k}{$v_name} =  $tmp ;
         }
         return;
     };
@@ -137,4 +156,40 @@ sub cast {
     );
 }
 
+sub merge {
+    my ($x, $y, %opt) = @_;
+
+    my @raw = (
+        { data => $x, by => $opt{by_x} || $opt{by}, 
+            value => $opt{value_x} || $opt{value} || [ 0 .. $#{$x->[0]} ],  }, 
+        { data => $y, by => $opt{by_y} || $opt{by}, 
+            value => $opt{value_y} || $opt{value} || [ 0 .. $#{$y->[0]} ],  }, 
+    );
+
+    my %main;
+    my @cut_list;
+    for my $i (0 .. $#raw){
+        my ($d, $by) = @{$raw[$i]}{qw/data by/};
+        for my $row (@$d) {
+            my $cut = join($opt{sep}, @{$row}[ @$by ]);
+            push @cut_list, $cut unless ( exists $main{$cut} );
+            $main{$cut}[$i] = $row;
+        }
+    }
+    @cut_list = sort @cut_list;
+
+    my @result;
+    for my $cut (@cut_list) {
+        my @vs  = split qr/$opt{sep}/, $cut;
+        for my $i (0 .. $#raw){
+            my $d = $main{$cut}[$i];
+            my $vlist = $raw[$i]{value};
+
+            push @vs, $d ? ($d->[$_] // '') : '' for (@$vlist);
+        }
+        push @result, \@vs;
+    }
+
+    return \@result;
+}
 1;
